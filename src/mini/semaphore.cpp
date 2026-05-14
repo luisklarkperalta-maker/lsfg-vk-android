1c9f7599f90889498c8baaf7a38e2fc5e00752cd
luisklarkperalta-maker <luisklarkperalta@gmail.com>
2026-05-15 03:28:07 +0800

    Update semaphore.cpp

--- src/mini/semaphore.cpp
+++ src/mini/semaphore.cpp
@@ -6,57 +6,47 @@
 
 #include <memory>
 
-using namespace Mini;
+using namespace Mini ;
 
 Semaphore::Semaphore(VkDevice device) {
-    // create semaphore
-    const VkSemaphoreCreateInfo desc{
-        .sType = VK_STRUCTURE_TYPE_SEMAPHORE_CREATE_INFO
-    };
-    VkSemaphore semaphoreHandle{};
-    auto res = Layer::ovkCreateSemaphore(device, &desc, nullptr, &semaphoreHandle);
-    if (res != VK_SUCCESS || semaphoreHandle == VK_NULL_HANDLE)
-        throw LSFG::vulkan_error(res, "Unable to create semaphore");
-
-    // store semaphore in shared ptr
-    this->semaphore = std::shared_ptr<VkSemaphore>(
-        new VkSemaphore(semaphoreHandle),
-        [dev = device](VkSemaphore* semaphoreHandle) {
-            Layer::ovkDestroySemaphore(dev, *semaphoreHandle, nullptr);
-        }
-    );
-}
-
-Semaphore::Semaphore(VkDevice device, int* fd) {
-    // create semaphore
     const VkExportSemaphoreCreateInfo exportInfo{
         .sType = VK_STRUCTURE_TYPE_EXPORT_SEMAPHORE_CREATE_INFO,
-        .handleTypes = VK_EXTERNAL_SEMAPHORE_HANDLE_TYPE_OPAQUE_FD_BIT
+        .pNext = nullptr,
+        .handleTypes = VK_EXTERNAL_SEMAPHORE_HANDLE_TYPE_SYNC_FD_BIT
     };
     const VkSemaphoreCreateInfo desc{
         .sType = VK_STRUCTURE_TYPE_SEMAPHORE_CREATE_INFO,
         .pNext = &exportInfo
     };
+
     VkSemaphore semaphoreHandle{};
     auto res = Layer::ovkCreateSemaphore(device, &desc, nullptr, &semaphoreHandle);
-    if (res != VK_SUCCESS || semaphoreHandle == VK_NULL_HANDLE)
-        throw LSFG::vulkan_error(res, "Unable to create semaphore");
 
-    // export semaphore to fd
-    const VkSemaphoreGetFdInfoKHR fdInfo{
-        .sType = VK_STRUCTURE_TYPE_SEMAPHORE_GET_FD_INFO_KHR,
-        .semaphore = semaphoreHandle,
-        .handleType = VK_EXTERNAL_SEMAPHORE_HANDLE_TYPE_OPAQUE_FD_BIT
-    };
-    res = Layer::ovkGetSemaphoreFdKHR(device, &fdInfo, fd);
-    if (res != VK_SUCCESS || *fd < 0)
-        throw LSFG::vulkan_error(res, "Unable to export semaphore to fd");
+    if (res != VK_SUCCESS)
+        throw LSFG::vulkan_error(res, "Unable to create binary semaphore");
 
-    // store semaphore in shared ptr
     this->semaphore = std::shared_ptr<VkSemaphore>(
         new VkSemaphore(semaphoreHandle),
-        [dev = device](VkSemaphore* semaphoreHandle) {
-            Layer::ovkDestroySemaphore(dev, *semaphoreHandle, nullptr);
+        [dev = device](VkSemaphore* h) {
+            Layer::ovkDestroySemaphore(dev, *h, nullptr);
         }
     );
+}
+
+void Semaphore::exportSyncFd(VkDevice device, int* fd) {
+    const VkSemaphoreGetFdInfoKHR fdInfo{
+        .sType = VK_STRUCTURE_TYPE_SEMAPHORE_GET_FD_INFO_KHR,
+        .semaphore = *this->semaphore,
+        .handleType = VK_EXTERNAL_SEMAPHORE_HANDLE_TYPE_SYNC_FD_BIT
+    };
+
+    // FIXED: Correct check for function pointer existence to silence warning
+    if (&Layer::ovkGetSemaphoreFdKHR == nullptr) {
+        throw std::runtime_error("ovkGetSemaphoreFdKHR is NULL");
+    }
+
+    auto res = Layer::ovkGetSemaphoreFdKHR(device, &fdInfo, fd);
+    if (res != VK_SUCCESS || *fd < 0) {
+        *fd = -1;
+    }
 }
